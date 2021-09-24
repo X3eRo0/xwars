5942051b2b204e4f3f6c216453e52d40dc81ed77
X3eRo0 <psinghania256@gmail.com>
2021-09-24 23:38:33 +0530

    kamikaze wins

--- bots/kamikaze.asm
+++ bots/kamikaze.asm
@@ -3,11 +3,12 @@
 	mov $r0, $r3
 	mov $r1, $r0
 	and $r1, #0xfffff000
+	lea $r6, [$r1+0x40]
 	lea $sp, [$r1+#0x3a0]
 	lea $r2, [$r1+#0x400] ; upper bound
 	
-	mov $rc, #0x0f0f1145 ; kamikaze opcode:
-	mov $rb, #0x020f1106
+	mov $rc, #0x060f1142 ; kamikaze opcode:
+	mov $rb, #0x020f1115
 	mov $ra, #0x003c003c
 	mov $r9, #0x003c003c
 	mov $r8, #0x000f0146
@@ -22,8 +23,8 @@ _start:
 	jmp $sp
 
 _kamikaze:
-	test $sp, $sp
-	cmovz $sp, $r2
+	cmp $sp, $r6	
+	cmovb $sp, $r2
 	pusha
 	pusha
 	pusha
